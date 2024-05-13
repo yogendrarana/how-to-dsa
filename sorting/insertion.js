221b1a5d68a97c40cee7f1baa6f32bf1552c3cd8
Yogendra Rana <yogendrarana.tech@gmail.com>
2024-05-13 08:01:09 +0545

    Changes

---
+++ sorting/insertion.js
@@ -0,0 +1,42 @@
+/*
+    Algorithm:
+    1. Start from the second element of the array and compare it with the previous element.
+    2. If the current element is smaller than the previous element, compare it with the elements before.
+    3. Move the greater elements one position up to make space for the current element.
+    4. Repeat step 2 and 3 until the current element is greater than the previous element.
+    5. Repeat steps 1 to 4 for the remaining elements of the array.
+*/
+
+/*
+    Time Complexity:
+    Best Case Time Complexity: O(n)
+    Average Case Time Complexity: O(n^2)
+    Worst Case Time Complexity: O(n^2)
+
+    Space Complexity: O(1)
+    In-Place: Yes
+    Stable: Yes
+*/
+
+function inserttionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+
+        const currentElement = arr[i];
+
+        let j = i - 1;
+
+        while (j >= 0 && arr[j] > currentElement) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = currentElement;
+    }
+
+    return arr;
+
+}
+
+const arr = [5, 3, 8, 1, 9, 6, 7];
+const sortedArray = inserttionSort(arr);
+console.log("sortedArray: ", sortedArray);
---
+++ sorting/merge.js
@@ -0,0 +1,28 @@
+/*
+    Merge Sort Algorithm
+    1. Divide the array into two halves.
+    2. Recursively sort the two halves.
+    3. Merge the two halves.
+*/
+
+/*
+    Time Complexity:
+    Best Case Time Complexity: O(n log n)
+    Average Case Time Complexity: O(n log n)
+    Worst Case Time Complexity: O(n log n)
+
+    Space Complexity: O(n)
+    In-Place: No
+    Stable: Yes
+*/
+
+
+
+function mergeSort(arr) {
+
+}
+
+const arr = [5, 3, 8, 1, 9, 6, 7];
+const sortedArray = mergeSort(arr);
+
+console.log("sortedArray: ", sortedArray);
